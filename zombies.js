f025cf2d87205511f2caa75cf6613f8c54b94b71
jeffmon <jeffmon@gmail.com>
2017-06-05 23:07:39 -1000

    finished RangedZombie class

--- zombies.js
+++ zombies.js
@@ -167,51 +167,13 @@ StrongZombie.prototype  = Object.create(Zombie.prototype, {
   constructor: StrongZombie
 });
 
+function RangedZombie(health, strength, speed){
+  Zombie.call(this, health, strength, speed);
+}
 
-/**
- * Class => StrongZombie(health, strength, speed)
- * -----------------------------
- * Creates a strong zombie.
- *
- * The StrongZombie class constructor will call
- *   the super class (Zombie) constructor
- *   while passing in the 3 Zombie constructor params
- *
- * @name StrongZombie
- * @param {number} health           The zombie's health.
- * @param {number} strength         The zombie's strength.
- * @param {number} speed            The zombie's speed.
- */
-
-
-/**
- * StrongZombie Extends Zombie Class
- * -----------------------------
- */
-
-
-
-/**
- * Class => RangedZombie(health, strength, speed)
- * -----------------------------
- * Creates a ranged zombie.
- *
- * The RangedZombie class constructor will call
- *   the super class (Zombie) constructor
- *   while passing in the 3 Zombie constructor params
- *
- * @name RangedZombie
- * @param {number} health           The zombie's health.
- * @param {number} strength         The zombie's strength.
- * @param {number} speed            The zombie's speed.
- */
-
-
-/**
- * RangedZombie Extends Zombie Class
- * -----------------------------
- */
-
+RangedZombie.prototype  = Object.create(Zombie.prototype, {
+  constructor: RangedZombie
+});
 
 
 /**
