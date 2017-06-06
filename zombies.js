fe52c1c40f790f20fa123359431b681e933920f7
jeffmon <jeffmon@gmail.com>
2017-06-05 23:09:17 -1000

    finshex exercise

--- zombies.js
+++ zombies.js
@@ -175,28 +175,13 @@ RangedZombie.prototype  = Object.create(Zombie.prototype, {
   constructor: RangedZombie
 });
 
+function ExplodingZombie(health, strength, speed){
+  Zombie.call(this, health, strength, speed);
+}
 
-/**
- * Class => ExplodingZombie(health, strength, speed)
- * -----------------------------
- * Creates an exploding zombie.
- *
- * The ExplodingZombie class constructor will call
- *   the super class (Zombie) constructor
- *   while passing in the 3 Zombie constructor params
- *
- * @name ExplodingZombie
- * @param {number} health           The zombie's health.
- * @param {number} strength         The zombie's strength.
- * @param {number} speed            The zombie's speed.
- */
-
-
-/**
- * ExplodingZombie Extends Zombie Class
- * -----------------------------
- */
-
+ExplodingZombie.prototype  = Object.create(Zombie.prototype, {
+  constructor: ExplodingZombie
+});
 
 
 
